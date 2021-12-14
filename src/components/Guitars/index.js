ceb73de280e4ce64e4420af07f39c53be29fd711
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-12-14 13:23:11 -0600

    Estructura basica de FrontEnd

---
+++ src/components/Guitars/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Guitars = () => {
+    return (
+        <div>
+            Listado de Guitarras
+        </div>
+    )
+}
+
+export default Guitars
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            Este es el Header
+        </div>
+    )
+}
+
+export default Header
